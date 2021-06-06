7a7475d4fd7e44c91f4f233e01fad2410c9bac8c
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-06-06 19:17:00 +0530

    coming soon

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const hourEl = document.querySelector(".hour");
+console.log("1");
+const minuteEl = document.querySelector(".minute");
+// const secondEl = document.querySelector(".second");
+
+function setTime() {
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  //   const seconds = time.getSeconds();
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hours,
+    0,
+    12,
+    0,
+    360
+  )}deg)`;
+
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
+
+  //   secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+  //     seconds,
+  //     0,
+  //     60,
+  //     0,
+  //     360
+  //   )}deg)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+setTime();
+
+setInterval(setTime, 60000);
