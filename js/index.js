0500b096b29abd7b8c5afee9ada190285ff29ec7
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-06-07 01:25:29 +0530

    contact us responsive

--- js/index.js
+++ js/index.js
@@ -1,32 +1,58 @@
-const hourEl = document.querySelector(".hour");
-console.log("1");
-const minuteEl = document.querySelector(".minute");
+////////////////////////////////////////////////////////////////
+//////////// FIXED NAVIGATION BAR AND SOCIAL BAR 
+////////////////////////////////////////////////////////////////
 
-function setTime() {
-  const time = new Date();
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
+////////////////////////////////////////////////////////////////
+//////////// SOCIAL BAR CLOSE OPEN BUTTON 
+////////////////////////////////////////////////////////////////
 
-  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
-    hours,
-    0,
-    12,
-    0,
-    360
-  )}deg)`;
+////////////////////////////////////////////////////////////////
+//////////// START YOUR JOURNEY BUTTON CLICK
+////////////////////////////////////////////////////////////////
 
-  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
-    minutes,
-    0,
-    60,
-    0,
-    360
-  )}deg)`;
-}
+////////////////////////////////////////////////////////////////
+//////////// SERVICES SLIDER BUTTON CLICK
+////////////////////////////////////////////////////////////////
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
-setTime();
+////////////////////////////////////////////////////////////////
+//////////// TESTIMONIAL SLIDER BUTTON CLICK
+////////////////////////////////////////////////////////////////
 
-setInterval(setTime, 60000);
+////////////////////////////////////////////////////////////////
+//////////// READ MORE BUTTON CLICK
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+//////////// POP-UP CLOCK JS
+////////////////////////////////////////////////////////////////
+// const hourEl = document.querySelector(".hour");
+// const minuteEl = document.querySelector(".minute");
+
+// function setTime() {
+//   const time = new Date();
+//   const hours = time.getHours();
+//   const minutes = time.getMinutes();
+
+//   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+//     hours,
+//     0,
+//     12,
+//     0,
+//     360
+//   )}deg)`;
+
+//   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+//     minutes,
+//     0,
+//     60,
+//     0,
+//     360
+//   )}deg)`;
+// }
+
+// const scale = (num, in_min, in_max, out_min, out_max) => {
+//   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+// };
+// setTime();
+
+// setInterval(setTime, 60000);
