0a901006fd17ef9b16555356a3d047d54489d1ab
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-06-09 02:56:43 +0530

    js

--- js/index.js
+++ js/index.js
@@ -83,34 +83,34 @@ function serviceSlider() {
 ////////////////////////////////////////////////////////////////
 //////////// POP-UP CLOCK JS
 ////////////////////////////////////////////////////////////////
-// const hourEl = document.querySelector(".hour");
-// const minuteEl = document.querySelector(".minute");
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
 
-// function setTime() {
-//   const time = new Date();
-//   const hours = time.getHours();
-//   const minutes = time.getMinutes();
+function setTime() {
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
 
-//   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
-//     hours,
-//     0,
-//     12,
-//     0,
-//     360
-//   )}deg)`;
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hours,
+    0,
+    12,
+    0,
+    360
+  )}deg)`;
 
-//   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
-//     minutes,
-//     0,
-//     60,
-//     0,
-//     360
-//   )}deg)`;
-// }
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
+}
 
-// const scale = (num, in_min, in_max, out_min, out_max) => {
-//   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-// };
-// setTime();
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+setTime();
 
-// setInterval(setTime, 60000);
+setInterval(setTime, 60000);
