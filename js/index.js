fc0c4297dd83663d586ad1660205e1408effdd6c
Atharva-Shandilya <atharvashandilya886@gmail.com>
2021-06-09 01:33:44 +0530

    service slider

--- js/index.js
+++ js/index.js
@@ -6,17 +6,17 @@ const hamburgerMenu = document.querySelector(".hamburger__menu");
 const hamburgerNav = document.querySelectorAll(".hamburger");
 const navigationItem = document.querySelectorAll(".navigation__item");
 
+function responsiveNav() {
+  hamburgerNav.forEach((item) => {
+    item.classList.toggle("open");
+  });
+}
+
 hamburgerMenu.addEventListener("click", responsiveNav);
 
 navigationItem.forEach((item) => {
   item.addEventListener("click", responsiveNav);
 });
-
-function responsiveNav() {
-  hamburgerNav.forEach((item) => {
-    item.classList.toggle('open')
-  })
-}
 
 ////////////////////////////////////////////////////////////////
 //////////// SOCIAL BAR CLOSE OPEN BUTTON
@@ -29,7 +29,49 @@ function responsiveNav() {
 ////////////////////////////////////////////////////////////////
 //////////// SERVICES SLIDER BUTTON CLICK
 ////////////////////////////////////////////////////////////////
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+const serviceGridContainer = document.querySelector(".grid-container");
+let maxArrowClickable = null;
+let arrowClicked = 0;
+let rightClicked = null;
 
+function windowSizeUpdate() {
+  if (window.screen.width < 550) {
+    maxArrowClickable = 3;
+  } else {
+    maxArrowClickable = 1;
+  }
+  serviceGridContainer.style.transform = `translateX(0%)`
+}
+
+window.addEventListener("resize", windowSizeUpdate);
+windowSizeUpdate();
+
+arrowRight.addEventListener("click", () => {
+  rightClicked = true;
+  serviceSlider()
+});
+
+arrowLeft.addEventListener("click", () => {
+  rightClicked = false;
+  serviceSlider();
+});
+
+function serviceSlider() {
+  if (rightClicked) {
+    arrowClicked++;
+    if (arrowClicked > maxArrowClickable) {
+      arrowClicked = 0;
+    }
+  } else {
+    arrowClicked--;
+    if (arrowClicked < 0) {
+      arrowClicked = maxArrowClickable;
+    }
+  }
+  serviceGridContainer.style.transform = `translateX(${arrowClicked * -110}%)`;
+}
 ////////////////////////////////////////////////////////////////
 //////////// TESTIMONIAL SLIDER BUTTON CLICK
 ////////////////////////////////////////////////////////////////
