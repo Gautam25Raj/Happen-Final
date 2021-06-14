dbe4bd6f145c7bbc36c4772dbd930e9bda21f643
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-06-14 22:20:01 +0530

    JS for Increment Counter

--- js/index.js
+++ js/index.js
@@ -24,7 +24,6 @@ const fixedNav = function (entries) {
 };
 
 function remove(entries) {
-  console.log(entries);
   const [entry] = entries;
 
   if (entry.isIntersecting) {
@@ -91,7 +90,7 @@ const revealSection = function (entries, oberserver) {
   oberserver.unobserve(entry.target);
 };
 
-if (screenSize > 700) {
+if (screenSize > 550) {
   const sectionObserver = new IntersectionObserver(revealSection, {
     root: null,
     threshold: 0.4,
@@ -103,7 +102,7 @@ if (screenSize > 700) {
   });
 }
 
-if (screenSize < 700) {
+if (screenSize < 550) {
   const sectionObserverM = new IntersectionObserver(revealSection, {
     root: null,
     threshold: 0.3,
@@ -162,6 +161,7 @@ function serviceSlider() {
   }
   serviceGridContainer.style.transform = `translateX(${arrowClicked * -110}%)`;
 }
+
 ////////////////////////////////////////////////////////////////
 //////////// TESTIMONIAL SLIDER BUTTON CLICK
 ////////////////////////////////////////////////////////////////
@@ -226,3 +226,83 @@ const readMoreBtns = document.querySelectorAll(".read-more");
 readMoreBtns.forEach((readMore) => {
   readMore.addEventListener("click", comingSoon);
 });
+
+////////////////////////////////////////////////////////////////
+//////////// Increment Counter
+////////////////////////////////////////////////////////////////
+const counters = document.querySelectorAll(".counter-count");
+const counterContainer = document.querySelector(".counters");
+let counterObserver, counterObserverC;
+
+const increment = function (entries) {
+  const [entry] = entries;
+  let num = entry.target.innerText;
+  if (entry.isIntersecting) {
+    entry.target.innerText = "0";
+
+    const updateCounter = () => {
+      const target = +entry.target.getAttribute("data-target");
+      const c = +entry.target.innerText;
+
+      const increment = target / 100;
+
+      if (c < target) {
+        entry.target.innerText = `${Math.ceil(c + increment)}`;
+        setTimeout(updateCounter, 50);
+      } else {
+        entry.target.innerText = target;
+      }
+    };
+    counterObserver.unobserve(entry.target);
+    updateCounter();
+  }
+};
+
+const incrementC = function (entries) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) {
+    counters.forEach((counter) => {
+      counter.innerHTML = "0";
+
+      const updateCounterC = () => {
+        const target = +counter.getAttribute("data-target");
+        const c = +counter.innerText;
+
+        const increment = target / 100;
+
+        if (c < target) {
+          counter.innerText = `${Math.ceil(c + increment)}`;
+          setTimeout(updateCounterC, 30);
+        } else {
+          counter.innerText = target;
+        }
+      };
+      counterObserverC.unobserve(entry.target);
+      updateCounterC();
+    });
+    // entry.target.innerText = "0";
+  } else {
+    counters.forEach((counter) => {
+      counter.innerHTML = "0";
+    });
+  }
+};
+
+if (screenSize < 550) {
+  counterObserver = new IntersectionObserver(increment, {
+    root: null,
+    threshold: 0.2,
+  });
+
+  counters.forEach((counter) => {
+    counterObserver.observe(counter);
+  });
+} else {
+  counterObserverC = new IntersectionObserver(incrementC, {
+    root: null,
+    threshold: 0.2,
+  });
+
+  counterObserverC.observe(counterContainer);
+}
