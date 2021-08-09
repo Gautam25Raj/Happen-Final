e529343c91ff2154202c08561f8d3425039b1dac
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-08-09 16:41:22 +0530

    form handler
    PHP code to handle form

---
+++ form-handler.php
@@ -0,0 +1,42 @@
+<?php
+    if(isset($_POST['useremail']) && $_POST['useremail'] != ''){
+        $name = $_POST["username"];
+        $visitor_email = $_POST["useremail"];
+        $mobile = $_POST["usermobile"];
+
+        $email_from = "form@happenwedding.com";
+
+        $email_subject = "New Form Submission";
+
+        $to = "contact@happenwedding.com";
+    
+        $body = "User Name: $name.\n"."User Email: $visitor_email.\n"."User Mobile Number: $mobile.\n";
+    
+
+        $headers = "From: $email_from \r\n";
+
+        mail($to,$email_subject,$body,$headers);
+    }
+    
+    if(isset($_POST['fEmail']) && $_POST['fEmail'] != ''){
+        $first_name = $_POST["firstName"];
+        $second_name = $_POST["secondName"];
+        $sender_email = $_POST["fEmail"];
+        $contact = $_POST["fContact"];
+        $message = $_POST["fAsk"];
+
+        $from_email = "form@happenwedding.com";
+
+        $subject = "New Form Submission";
+
+        $to = "contact@happenwedding.com";
+    
+        $body_form = "User Name: $first_name"."$second_name.\n"."User Email: $sender_email.\n"."User Mobile Number: $contact.\n\n"."Message: $message";
+
+        $headers = "From: $from_email \r\n";
+
+        mail($to,$subject,$body_form,$headers);
+    }
+
+    header("Location: index.html");
+?>
